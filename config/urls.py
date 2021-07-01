ae21f6a437880b9925b19f01a6fb1d59a2ef3fa1
kolaider <kolja87@tut.by>
2021-07-01 19:34:05 +0300

    Covfigured mediafiles

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
 
 from vacancies.views import ListVacanciesByCompanyView, DetailVacancyView
 from vacancies.views import ListVacanciesView, ListVacanciesBySpecialtyView
